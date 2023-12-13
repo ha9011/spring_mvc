d66d3440f36997e8b903f121f6d9d9443736557f
ha9011 <gkehddnjs90@gmail.com>
2023-12-13 15:39:28 +0900

    순수 서블릿, 서블릿을 이용한 웹어플

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -5,24 +5,24 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         System.out.println("HelloServlet.service");
-        System.out.println("req = " + req);
-        System.out.println("resp = " + resp);
-        String username = req.getParameter("username");
-        System.out.println("username : " + username);
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
 
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("utf-8");
-        resp.getWriter().write("hello "+ username);
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
 
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
 
     }
 }
